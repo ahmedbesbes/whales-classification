3f24f02eab9973502a3f325b0db1c83209b290ea
Ahmed Besbes <ahmed.besbes@hotmail.com>
2020-02-25 00:57:16 +0100

    comment line

--- yolov3/utils/torch_utils.py
+++ yolov3/utils/torch_utils.py
@@ -34,7 +34,7 @@ def select_device(device='', apex=False, batch_size=None):
             if i == 1:
                 s = ' ' * len(s)
             # print("%sdevice%g _CudaDeviceProperties(name='%s', total_memory=%dMB)" %
-                  (s, i, x[i].name, x[i].total_memory / c))
+                #   (s, i, x[i].name, x[i].total_memory / c))
     else:
         print('Using CPU')
 
@@ -46,25 +46,25 @@ def fuse_conv_and_bn(conv, bn):
     # https://tehnokv.com/posts/fusing-batchnorm-and-conv/
     with torch.no_grad():
         # init
-        fusedconv=torch.nn.Conv2d(conv.in_channels,
+        fusedconv = torch.nn.Conv2d(conv.in_channels,
                                     conv.out_channels,
-                                    kernel_size = conv.kernel_size,
-                                    stride = conv.stride,
-                                    padding = conv.padding,
-                                    bias = True)
+                                    kernel_size=conv.kernel_size,
+                                    stride=conv.stride,
+                                    padding=conv.padding,
+                                    bias=True)
 
         # prepare filters
-        w_conv=conv.weight.clone().view(conv.out_channels, -1)
-        w_bn=torch.diag(bn.weight.div(torch.sqrt(bn.eps + bn.running_var)))
+        w_conv = conv.weight.clone().view(conv.out_channels, -1)
+        w_bn = torch.diag(bn.weight.div(torch.sqrt(bn.eps + bn.running_var)))
         fusedconv.weight.copy_(
             torch.mm(w_bn, w_conv).view(fusedconv.weight.size()))
 
         # prepare spatial bias
         if conv.bias is not None:
-            b_conv=conv.bias
+            b_conv = conv.bias
         else:
-            b_conv=torch.zeros(conv.weight.size(0))
-        b_bn=bn.bias - \
+            b_conv = torch.zeros(conv.weight.size(0))
+        b_bn = bn.bias - \
             bn.weight.mul(bn.running_mean).div(
                 torch.sqrt(bn.running_var + bn.eps))
         fusedconv.bias.copy_(
@@ -73,35 +73,36 @@ def fuse_conv_and_bn(conv, bn):
         return fusedconv
 
 
-def model_info(model, report = 'summary'):
+def model_info(model, report='summary'):
     # Plots a line-by-line description of a PyTorch model
-    n_p=sum(x.numel() for x in model.parameters())  # number parameters
-    n_g=sum(x.numel() for x in model.parameters()
-            if x.requires_grad)  # number gradients
+    n_p = sum(x.numel() for x in model.parameters())  # number parameters
+    n_g = sum(x.numel() for x in model.parameters()
+              if x.requires_grad)  # number gradients
     if report is 'full':
         print('%5s %40s %9s %12s %20s %10s %10s' %
               ('layer', 'name', 'gradient', 'parameters', 'shape', 'mu', 'sigma'))
         for i, (name, p) in enumerate(model.named_parameters()):
-            name=name.replace('module_list.', '')
+            name = name.replace('module_list.', '')
             print('%5g %40s %9s %12g %20s %10.3g %10.3g' %
                   (i, name, p.requires_grad, p.numel(), list(p.shape), p.mean(), p.std()))
     print('Model Summary: %g layers, %g parameters, %g gradients' %
           (len(list(model.parameters())), n_p, n_g))
 
 
-def load_classifier(name ='resnet101', n=2):
+def load_classifier(name='resnet101', n=2):
     # Loads a pretrained model reshaped to n-class output
     # https://github.com/Cadene/pretrained-models.pytorch#torchvision
     import pretrainedmodels
-    model = pretrainedmodels.__dict__[name](num_classes =1000, pretrained='imagenet')
+    model = pretrainedmodels.__dict__[name](
+        num_classes=1000, pretrained='imagenet')
 
     # Display model properties
     for x in ['model.input_size', 'model.input_space', 'model.input_range', 'model.mean', 'model.std']:
         print(x + ' =', eval(x))
 
     # Reshape output to n classes
-    filters= model.last_linear.weight.shape[1]
-    model.last_linear.bias= torch.nn.Parameter(torch.zeros(n))
-    model.last_linear.weight= torch.nn.Parameter(torch.zeros(n, filters))
-    model.last_linear.out_features= n
+    filters = model.last_linear.weight.shape[1]
+    model.last_linear.bias = torch.nn.Parameter(torch.zeros(n))
+    model.last_linear.weight = torch.nn.Parameter(torch.zeros(n, filters))
+    model.last_linear.out_features = n
     return model
