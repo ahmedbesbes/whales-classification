16d79115d1d0518b81b0092e7d2aa6f2e2e81e36
Ahmed Besbes <ahmed.besbes@hotmail.com>
2020-04-02 12:02:18 +0200

    more augmentation

--- dataloader.py
+++ dataloader.py
@@ -75,12 +75,12 @@ def augmentation(image_size, train=True, heavy=False):
                     A.HueSaturationValue(hue_shift_limit=20, p=1),
                 ]),
 
-                # A.OneOf([
-                #     A.CLAHE(clip_limit=2),
-                #     A.IAASharpen(),
-                #     A.IAAEmboss(),
-                # ],
-                #     p=0.1),
+                A.OneOf([
+                    A.CLAHE(clip_limit=2),
+                    A.IAASharpen(),
+                    A.IAAEmboss(),
+                ],
+                    p=0.1),
 
                 A.IAAPerspective(p=0.3),
 
@@ -90,7 +90,7 @@ def augmentation(image_size, train=True, heavy=False):
                             shear=0.2,
                             p=1),
 
-                #A.Cutout(num_holes=1, max_h_size=100, max_w_size=200, p=0.2),
+                A.Cutout(num_holes=1, max_h_size=100, max_w_size=200, p=0.2),
 
                 A.Resize(image_size, image_size),
                 A.Normalize(mean=[0.485, 0.456, 0.406],
